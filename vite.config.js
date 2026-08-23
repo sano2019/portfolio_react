0be045591085906542c88f26bf71295001e4d75a
Sander Nobel <sandernobel@Sanders-MacBook-Pro.local>
2026-08-23 17:34:11 +0700

    Updated vite config for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [
     react(),
     basicSsl(), // Automatically creates and caches local self-signed certificates
